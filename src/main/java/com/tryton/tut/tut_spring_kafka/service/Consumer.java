d5ab70f6b7f729f778766845b4405a751ac932d7
mephisto <mephisto2120@gmail.com>
2022-11-20 17:30:45 +0100

    First kafka consumer

---
+++ src/main/java/com/tryton/tut/tut_spring_kafka/service/Consumer.java
@@ -0,0 +1,16 @@
+package com.tryton.tut.tut_spring_kafka.service;
+
+import lombok.extern.apachecommons.CommonsLog;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@CommonsLog
+public class Consumer {
+    @KafkaListener(topics = "users", groupId = "group_id")
+    public void consume(String message) {
+        log.info(String.format("#### -&gt; Consumed message -&gt; %s", message));
+    }
+}
